f9d7e3407b6c7c03420bf8a290c9c83fa7257b9c
brucee63 <brucee63@gmail.com>
2016-09-08 21:31:42 -0500

    test

---
+++ README.md
@@ -0,0 +1,19 @@
+## The following files were contrbuted by the student.
+* 'README.md': This file, outlines all files used in this dataset analysis.
+* 'CodeBook.md': Listing of variable names/labels, codes, formats, datatypes
+* 'run_analysis.R': R file responsible for creating a tidy dataset combining training and testing data, joining multiple data files together. Also creating a summary analysis.
+  * Merges the training and the test sets to create one data set.
+  * Extracts only the measurements on the mean and standard deviation for each measurement.
+  * Uses descriptive activity names to name the activities in the data set
+  * Appropriately labels the data set with descriptive variable names.
+  * From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+## The following files were contributed by the study desginers and are referenced by the codebook and R analysis.
+* 'README.txt'
+* 'features_info.txt': Shows information about the variables used on the feature vector.
+* 'features.txt': List of all features.
+* 'activity_labels.txt': Links the class labels with their activity name.
+* 'train/X_train.txt': Training set.
+* 'train/y_train.txt': Training labels.
+* 'test/X_test.txt': Test set.
+* 'test/y_test.txt': Test labels.
---
+++ run_analysis.R
@@ -0,0 +1,70 @@
+getwd()
+setwd("c:/R/Course3/Week4/")
+rm(list = ls())
+cat("\014")
+
+?data.table
+
+#includes
+library(data.table)
+library(plyr)
+
+#read in the list of activities
+?read.table
+dtActivities <- data.table(read.table("./Project/data/activity_labels.txt", sep=" ", col.names = c("ActivityId","ActivityDescription")))
+
+#column names/variables for dataset
+dtFeatures <- data.table(read.table("./Project/data/features.txt", sep=" ", col.names = c("FeatureId","FeatureDescription")))
+
+dtSubjectTrain <- data.table(read.table("./Project/data/train/subject_train.txt", sep=" ", col.names = c("SubjectId")))
+dtSubjectTest <- data.table(read.table("./Project/data/test/subject_test.txt", sep=" ", col.names = c("SubjectId")))
+
+dtActivityTrain <- data.table(read.table("./Project/data/train/y_train.txt", sep=" ", col.names = c("ActivityId")))
+dtActivityTest <- data.table(read.table("./Project/data/test/y_test.txt", sep=" ", col.names = c("ActivityId")))
+
+# Step 4 in the provided instructions. 
+# Use the feature names to label the data columns.
+#one or more spaces
+dtFeaturesTrain <- data.table(read.table("./Project/data/train/X_train.txt", sep="", col.names = dtFeatures$FeatureDescription))
+dtFeaturesTest <- data.table(read.table("./Project/data/test/X_test.txt", sep="", col.names = dtFeatures$FeatureDescription))
+
+# Step 4 in the provided instructions. 
+#Add the subject and activties
+dtFeaturesTrain$SubjectId <- dtSubjectTrain$SubjectId
+dtFeaturesTrain$ActivityId <- dtActivityTrain$ActivityId
+dtFeaturesTest$SubjectId <- dtSubjectTest$SubjectId
+dtFeaturesTest$ActivityId <- dtActivityTest$ActivityId
+
+# Step 3 in the instructions provided.
+# Add the ActivityDescription via an inner join
+dtTrain <- merge(dtFeaturesTrain, dtActivities, all=TRUE, by=c("ActivityId"), sort = FALSE)
+dtTest <- merge(dtFeaturesTest, dtActivities, all=TRUE, by=c("ActivityId"), sort = FALSE)
+
+# Step 4 in the provided instructions. 
+dtTrain$sample.set <- rep("train", nrow(dtTrain))
+dtTest$sample.set <- rep("test", nrow(dtTest))
+
+# Step 1 in the instructions provided.
+#union the training and testing data.tables together. 
+dtAll <- rbind(dtTrain, dtTest)
+
+# Step 2 in the instructions provided. 
+# We want only the ActivityId, ActivityDescription, SubjectId, sample.set and any column with mean or std in the column name
+columnList <- grepl("^Activity|^Subject|\\.mean\\.|\\.std\\.|sample\\.set", names(dtAll))
+dtAllColSubset <- dtAll[,columnList, with = FALSE]
+names(dtAllColSubset)
+dtAllColSubset[1:3]
+
+# Step 5 creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+setkey(dtAllColSubset, ActivityDescription, SubjectId)
+
+
+df <- as.data.frame(dtAllColSubset)
+dfAgg <- aggregate(df[, 2:67], list(df$ActivityDescription, df$SubjectId), mean)
+
+head(dfAgg)
+
+dfAgg <- rename(dfAgg, c("Group.1"="ActivityDescription", "Group.2"="Subjectid"))
+
+df
+dfAgg
